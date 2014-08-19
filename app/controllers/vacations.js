41262595bc9741f84705874b685c98586bebf908
Joy Pratt <joympratt@gmail.com>
2014-08-19 15:29:16 -0500

    added upload and download

--- app/controllers/vacations.js
+++ app/controllers/vacations.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var Vacation = require('../models/vacation'),
-    moment   = require('moment');
+var Vacation  = require('../models/vacation'),
+    moment    = require('moment'),
+    mp        = require('multiparty');
 
 exports.index = function(req,res){
   Vacation.all(function(err, vacations){
@@ -16,5 +17,30 @@ exports.newVacation = function(req,res){
 exports.create = function(req,res){
   Vacation.create(req.body, function(){
     res.redirect('/vacations');
+  });
+};
+
+exports.show = function(req,res){
+  Vacation.findById(req.params.id, function(err, vacation){
+    res.render('vacations/show', {vacation:vacation, moment:moment});
+  });
+};
+
+exports.downloadPhoto = function(req,res){
+  Vacation.findById(req.params.id, function(err,vacation){
+    vacation.downloadPhoto(req.body.url, function(){
+      res.redirect('/vacations/' + req.params.id);
+    });
+  });
+};
+
+exports.uploadPhoto = function(req,res){
+  Vacation.findById(req.params.id, function(err,vacation){
+    var form = new mp.Form();
+    form.parse(req,function(err, fields, files){
+      vacation.uploadPhoto(files, function(){
+        res.redirect('/vacations/' + req.params.id);
+      });
+    });
   });
 };
--- app/models/vacation.js
+++ app/models/vacation.js
@@ -1,12 +1,18 @@
 'use strict';
 
+var Mongo = require('mongodb'),
+    _     = require('lodash'),
+    cp    = require('child_process'),
+    fs    = require('fs'),
+    path  = require('path');
+
 function Vacation(o){
   this.name   = o.name;
   this.start  = parseFloat(o.start);
   this.end    = parseFloat(o.end);
   this.lat    = parseFloat(o.lat);
   this.lng    = parseFloat(o.lng);
-  this.photo  = [];
+  this.photos = [];
 }
 
 Object.defineProperty(Vacation, 'collection', {
@@ -20,6 +26,49 @@ Vacation.all = function(cb){
 Vacation.create = function(o,cb){
   var vac = new Vacation(o);
   Vacation.collection.save(vac,cb);
+};
+
+Vacation.findById = function(id, cb){
+  var _id = Mongo.ObjectID(id);
+  Vacation.collection.findOne({_id:_id}, function(err,obj){
+    cb(err, _.create(Vacation.prototype, obj));
+  });
+};
+
+Vacation.prototype.uploadPhoto = function(files, cb){
+  var dir     = __dirname + '/../static/img/' + this._id,
+      exist   = fs.existsSync(dir),
+      self    = this;
+
+  if(!exist){
+    fs.mkdirSync(dir);
+  }
+
+  files.photos.forEach(function(photo){
+    var ext   = path.extname(photo.path),
+        rel   = '/img/' + self._id + '/' + self.photos.length + ext,
+        abs   = dir + '/' + self.photos.length + ext;
+    fs.renameSync(photo.path, abs);
+
+    self.photos.push(rel);
+  });
+
+  Vacation.collection.save(self, cb);
+};
+
+Vacation.prototype.downloadPhoto = function(url,cb){
+  var extensions  = url.split('.'),
+      extension   = extensions[extensions.length -1],
+      dir         = this._id,
+      file        = this.photos.length + '.' + extension,
+      self        = this;
+
+  cp.execFile(__dirname + '/../scripts/download.sh', [url, file, dir], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
+    var photo = '/img/' + dir + '/' + file;
+    self.photos.push(photo);
+    Vacation.collection.save(self, cb);
+
+  });
 };
 
 module.exports = Vacation;
